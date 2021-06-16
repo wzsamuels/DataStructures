5b5775e70f69fca47c8753743cb557a22c2e6e58
Zach Samuels <wzsamuels@gmail.com>
2021-06-16 15:53:34 -0400

    Added UnorderedLinkMap class and test class.

---
+++ DataStructuresProject/Map/UnorderedLinkedMap.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructures.PositionalList;
+
+namespace DataStructures.Map
+{
+    /**
+    * UnorderedLinkedMap extends AbstractMap to define the functionality for
+    * the Map ADT implemented as positional linked list.
+    * 
+    * @author Zach Samuels
+    *
+    * @param <K> Generic type used for the Map's keys.
+    * @param <V> Generic type stored as the Map's values.
+    */
+    public class UnorderedLinkedMap<TKey, TValue> : AbstractMap<TKey, TValue>
+    {
+		private IPositionalList<IEntry<TKey, TValue>> list;
+
+		/**
+		 * UnorderedLinkedMap constructor with no parameters. Creates a new, empty Map. 
+		 */
+		public UnorderedLinkedMap()
+		{
+			this.list = new PositionalLinkedList<IEntry<TKey, TValue>>();
+		}
+
+		/**
+		 * Finds the Entry with the given Key in the Map.
+		 * 
+		 * @param key The Key of the Entry to find.
+		 * 
+		 * @return The Position of the Key if it exists, null otherwise.
+		 */
+		private IPosition<IEntry<TKey, TValue>> LookUp(TKey key)
+		{
+			IEnumerator<IPosition<IEntry<TKey, TValue>>> iterator = list.PositionIterator();
+
+			while(iterator.MoveNext())
+			{
+				if (iterator.Current.GetElement().GetKey().Equals(key))
+					return iterator.Current;
+			}
+
+			return null;
+
+		}
+
+		/**
+		 * Gets the Value associated with the given Key.
+		 * 
+		 * @param key The Key to look up in the Map.
+		 * 
+		 * @return The Value associated with the Key or null if no such Key exists.
+		 */
+		public override TValue GetValue(TKey key)
+		{
+			IPosition<IEntry<TKey, TValue>> p = LookUp(key);
+
+			if (p != null && p.GetElement() != null)
+			{
+				MoveToFront(p);
+				return p.GetElement().GetValue();
+			}
+			else
+				return default;
+		}
+
+		/**
+		 * Moves the given Position to the front of the Map.
+		 * 
+		 * @param position The Position to move to the front of the Map.
+		 */
+		private void MoveToFront(IPosition<IEntry<TKey, TValue>> position)
+		{
+			list.Remove(position);
+			list.AddFirst(position.GetElement());
+		}
+
+		/**
+		 * Adds a Key associated with the given Value to the Map if no such Key exists, or replaces
+		 * the Value if the Key already exists. 
+		 * 
+		 * @param key The Key to add or set in the Map.
+		 * @param value The Value to add or replace.
+		 * 
+		 * @return The old Value associated with the Key or null if the Key is new.
+		 */
+		public override TValue Put(TKey key, TValue value)
+		{
+			IPosition<IEntry<TKey, TValue>> p = LookUp(key);
+
+			if (p == null)
+			{
+				list.AddFirst(new MapEntry<TKey, TValue>(key, value));
+				return default;
+			}
+			else
+			{
+				IEntry<TKey, TValue> temp = list.SetPosition(p, new MapEntry<TKey, TValue>(key, value));
+				MoveToFront(p);
+				return temp.GetValue();
+			}
+		}
+
+		/**
+		 * Removes and returns the Value associated with the given from the Map.
+		 * 
+		 * @param key The Key to remove from the Map.
+		 * 
+		 * @return The Value associated with the Key or null if no such Key exists.
+		 */
+		public override TValue Remove(TKey key)
+		{
+			IPosition<IEntry<TKey, TValue>> p = LookUp(key);
+
+			if (p == null)
+				return default;
+			else
+				return list.Remove(p).GetValue();
+		}
+
+		/**
+		 * Returns the number of entries currently in the Map.
+		 * 
+		 * @return The size of the Map.
+		 */
+		public override int Size()
+		{
+			return list.Size();
+		}
+
+		/**
+		 * Generates an object that can be iterated over to retrieve the Entries in
+		 * the Map.
+		 * 
+		 * @return An Iterable object for all Entries in the Map.
+		 */
+		public override IEnumerable<IEntry<TKey, TValue>> EntryIterator()
+		{
+			IPositionalList<IEntry<TKey, TValue>> set = new PositionalLinkedList<IEntry<TKey, TValue>>();
+			foreach (IEntry<TKey, TValue> m in list)
+			{
+				set.AddLast(m);
+			}
+			return set;
+		}
+
+		/**
+		 * Generates a String representation of the Map listing its Keys in their internally
+		 * stored order.
+		 * 
+		 * @return The Map's Keys as a String.
+		 */
+		public override string ToString()
+		{
+			StringBuilder sb = new(this.GetType().Name + "[");
+			IEnumerator<IEntry<TKey, TValue>> it = list.GetEnumerator();
+
+			if (it.MoveNext())
+			{
+				sb.Append(it.Current.GetKey());
+			}
+			while (it.MoveNext())
+			{
+				sb.Append(", ");
+				sb.Append(it.Current.GetKey());
+			}
+
+			sb.Append(']');
+			return sb.ToString();
+		}
+	}
+}
---
+++ DataStructuresTests/Map/UnorderedLinkedMapTest.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures.Map;
+using System;
+using System.Collections.Generic;
+
+namespace TestProject2.Map
+{
+    /**
+    * Test class for UnorderedLinkedMap.
+    * 
+    * @author Zach Samuels
+    */
+    [TestClass]
+    public class UnorderedLinkedMapTest
+    {
+		private IMap<int, string> map;
+
+		/**
+		 * Creates a new Map for testing.
+		 */
+		[TestInitialize]
+		public void SetUp()
+		{
+			map = new UnorderedLinkedMap<int, string>();
+		}
+
+		/**
+		 * Test method for Put().
+		 */
+		[TestMethod]
+		public void TestPut()
+		{
+			Assert.AreEqual(0, map.Size());
+			Assert.IsTrue(map.IsEmpty());
+			Assert.IsNull(map.Put(3, "string3"));
+			Assert.AreEqual("UnorderedLinkedMap`2[3]", map.ToString());
+			Assert.AreEqual(1, map.Size());
+			Assert.IsNull(map.Put(5, "string5"));
+			Assert.AreEqual("UnorderedLinkedMap`2[5, 3]", map.ToString());
+			Assert.AreEqual(2, map.Size());
+
+			Assert.AreEqual("string3", map.Put(3, "newstring3"));
+			Assert.AreEqual("newstring3", map.GetValue(3));
+			Assert.AreEqual("UnorderedLinkedMap`2[3, 5]", map.ToString());
+		}
+
+		/**
+		 * Test method for GetValue().
+		 */
+		[TestMethod]
+		public void TestGetValue()
+		{
+			Assert.IsTrue(map.IsEmpty());
+			Assert.IsNull(map.Put(3, "string3"));
+			Assert.IsNull(map.Put(5, "string5"));
+			Assert.IsNull(map.Put(2, "string2"));
+			Assert.IsNull(map.Put(4, "string4"));
+			Assert.IsNull(map.Put(1, "string1"));
+			Assert.IsFalse(map.IsEmpty());
+			Assert.AreEqual("UnorderedLinkedMap`2[1, 4, 2, 5, 3]", map.ToString());
+
+			Assert.AreEqual("string1", map.GetValue(1));
+			Assert.AreEqual("UnorderedLinkedMap`2[1, 4, 2, 5, 3]", map.ToString());
+			Assert.AreEqual("string3", map.GetValue(3));
+			Assert.AreEqual("UnorderedLinkedMap`2[3, 1, 4, 2, 5]", map.ToString());
+			Assert.AreEqual("string1", map.GetValue(1));
+			Assert.AreEqual("UnorderedLinkedMap`2[1, 3, 4, 2, 5]", map.ToString());
+
+			Assert.IsNull(map.GetValue(6));
+		}
+
+		/**
+		 * Test method for Remove().
+		 */
+		[TestMethod]
+		public void TestRemove()
+		{
+			Assert.IsTrue(map.IsEmpty());
+			Assert.IsNull(map.Put(3, "string3"));
+			Assert.IsNull(map.Put(5, "string5"));
+			Assert.IsNull(map.Put(2, "string2"));
+			Assert.IsNull(map.Put(4, "string4"));
+			Assert.IsNull(map.Put(1, "string1"));
+			Assert.IsFalse(map.IsEmpty());
+			Assert.AreEqual("UnorderedLinkedMap`2[1, 4, 2, 5, 3]", map.ToString());
+
+			Assert.IsNull(map.Remove(0));
+			Assert.AreEqual(5, map.Size());
+			Assert.IsFalse(map.IsEmpty());
+			Assert.AreEqual("UnorderedLinkedMap`2[1, 4, 2, 5, 3]", map.ToString());
+
+			Assert.AreEqual("string2", map.Remove(2));
+			Assert.AreEqual(4, map.Size());
+			Assert.AreEqual("UnorderedLinkedMap`2[1, 4, 5, 3]", map.ToString());
+		}
+
+		/**
+		 * Test method for GetEnumerator().
+		 */
+		[TestMethod]
+		public void TestIterator()
+		{
+			Assert.IsNull(map.Put(3, "string3"));
+			Assert.IsNull(map.Put(5, "string5"));
+			Assert.IsNull(map.Put(2, "string2"));
+			Assert.IsNull(map.Put(4, "string4"));
+			Assert.IsNull(map.Put(1, "string1"));
+
+			IEnumerator<int> it = map.GetEnumerator();
+			Assert.IsTrue(it.MoveNext());
+			Assert.AreEqual(1, (int)it.Current);
+			Assert.IsTrue(it.MoveNext());
+			Assert.AreEqual(4, (int)it.Current);
+			Assert.IsTrue(it.MoveNext());
+			Assert.AreEqual(2, (int)it.Current);
+			Assert.IsTrue(it.MoveNext());
+			Assert.AreEqual(5, (int)it.Current);
+			Assert.IsTrue(it.MoveNext());
+			Assert.AreEqual(3, (int)it.Current);
+		}
+
+		/**
+		 * Test method for entrySet().
+		 */
+		[TestMethod]
+		public void TestEntrySet()
+		{
+			Assert.IsNull(map.Put(3, "string3"));
+			Assert.IsNull(map.Put(5, "string5"));
+			Assert.IsNull(map.Put(2, "string2"));
+			Assert.IsNull(map.Put(4, "string4"));
+			Assert.IsNull(map.Put(1, "string1"));
+
+			IEnumerator<IEntry<int, string>> it = map.EntryIterator().GetEnumerator();
+			Assert.IsTrue(it.MoveNext());
+			IEntry<int, string> entry = it.Current;
+			Assert.AreEqual(1, entry.GetKey());
+			Assert.AreEqual("string1", (String)(entry.GetValue()));
+
+			Assert.IsTrue(it.MoveNext());
+			entry = it.Current;
+			Assert.AreEqual(4, (int)(entry.GetKey()));
+			Assert.AreEqual("string4", (String)(entry.GetValue()));
+
+			Assert.IsTrue(it.MoveNext());
+			entry = it.Current;
+			Assert.AreEqual(2, (int)(entry.GetKey()));
+			Assert.AreEqual("string2", (String)(entry.GetValue()));
+
+			Assert.IsTrue(it.MoveNext());
+			entry = it.Current;
+			Assert.AreEqual(5, (int)(entry.GetKey()));
+			Assert.AreEqual("string5", (String)(entry.GetValue()));
+
+			Assert.IsTrue(it.MoveNext());
+			entry = it.Current;
+			Assert.AreEqual(3, (int)(entry.GetKey()));
+			Assert.AreEqual("string3", (String)(entry.GetValue()));
+		}
+
+		/**
+		 * Test method for ValueIterator().
+		 */
+		[TestMethod]
+		public void TestValues()
+		{
+			Assert.IsNull(map.Put(3, "string3"));
+			Assert.IsNull(map.Put(5, "string5"));
+			Assert.IsNull(map.Put(2, "string2"));
+			Assert.IsNull(map.Put(4, "string4"));
+			Assert.IsNull(map.Put(1, "string1"));
+
+			IEnumerator<String> it = map.ValueIterator();
+			Assert.IsTrue(it.MoveNext());
+			Assert.AreEqual("string1", it.Current);
+			Assert.IsTrue(it.MoveNext());
+			Assert.AreEqual("string4", it.Current);
+			Assert.IsTrue(it.MoveNext());
+			Assert.AreEqual("string2", it.Current);
+			Assert.IsTrue(it.MoveNext());
+			Assert.AreEqual("string5", it.Current);
+			Assert.IsTrue(it.MoveNext());
+			Assert.AreEqual("string3", it.Current);		
+		}
+	}
+}
